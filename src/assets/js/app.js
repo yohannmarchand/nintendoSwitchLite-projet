ff24f07dc548508199f6b5e1497749c668038f4a
Yohann Marchand <marchandyohannnnn@gmail.com>
2020-04-02 22:36:55 +0200

    [css] carateristic

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -9,11 +9,16 @@
 var barsElement = document.getElementsByClassName("navigation__triger"),
     navElement = document.getElementsByClassName("navigation__box");
 
-
-barsElement.addEventListener("click" ,() => {
+barsElement[0].addEventListener("click" ,() => {
     
-    console.log('toto');
-    navElement.toggle("navigation__box--activ");
+    navElement[0].classList.toggle("navigation__box--activ");
 
+    if (barsElement[0].classList[1] == "fa-bars") {
+        barsElement[0].classList.replace("fa-bars","fa-times");
+    }
+    else{
+        barsElement[0].classList.replace("fa-times","fa-bars");
+    }
 
+    
 })
